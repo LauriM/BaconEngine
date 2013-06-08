2760a2e8db96fff1f3bc421894d11abc1e6bb8bc
Lauri Makinen <lauri.sakari.makinen@gmail.com>
2013-06-08 11:12:44 +0300

    Ctags is now working

--- BaconEngine/Player.cpp
+++ BaconEngine/Player.cpp
@@ -2,6 +2,9 @@
 
 #include "Player.h"
 
+/**
+ * Init the player and load the media
+ */
 bool Player::init(){
 	player = al_load_bitmap("player.png");
 
@@ -17,6 +20,15 @@ bool Player::init(){
 	return true;
 }
 
+/**
+ * Render the player
+ */
 void Player::render(){
 	al_draw_rotated_bitmap(player ,0,0 ,position.x ,position.y , 0.5 ,0);
+}
+
+/**
+ * Take the player input, calculate state and apply physics
+ */
+void Player::update(){
 }
